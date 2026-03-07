8f06adebd6b65858c7992dbfc8ebfb729c931044
Ayush kumar gupta <ayushkumargupta0441@gmail.com>
2026-03-07 20:00:03 +0530

    all updates

--- server/db.ts
+++ server/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 import * as schema from "@shared/schema";
 
-const { Pool } = pg;
+const { Pool } = pg; 
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
